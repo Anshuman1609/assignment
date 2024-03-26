19140a3fe3906311b5810bbc3a4e05f35c820b64
Anshuman1609 <anshumandubey1609@gmail.com>
2024-03-26 20:54:59 +0530

    href

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -17,7 +17,7 @@ const Blogs = () => {
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="/" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
           <div className="card" style={{width: '18rem'}}>
@@ -25,7 +25,7 @@ const Blogs = () => {
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="/" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
           <div className="card" style={{width: '18rem'}}>
@@ -33,7 +33,7 @@ const Blogs = () => {
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="/" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
