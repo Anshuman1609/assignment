168d9e961bad35ca85d0e4c38a2c46bc7f8f2aae
Anshuman1609 <anshumandubey1609@gmail.com>
2024-03-26 21:10:18 +0530

    allblog.js

---
+++ src/components/Allblog.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../styles/Blogs.css';
+import img2022 from '../imges/2022.jpg';
+import coding from '../imges/coding.jpg';
+import frag from '../imges/frag.jpg';
+
+const Allblog = () => {
+    return (
+        <>
+            <div className='container my-5'>
+                <div className="cards">
+                    <div className="card my-card" style={{ width: '20rem', marginBottom: '20px' }}>
+                        <img src={img2022} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-title">Private Equity</p>
+                            <p className="card-text">Introducing Campaigns</p>
+                            <p className="description-more"> With competition getting tougher in the private markets, becoming... </p>
+                        </div>
+                    </div>
+                    <div className="card" style={{ width: '20rem', marginBottom: '20px' }}>
+                        <img src={frag} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-title">Investment Banking</p>
+                            <p className="card-text">Introducing Podcasts</p>
+                            <p className="description-more"> With competition getting tougher in the private markets, becoming... </p>
+                        </div>
+                    </div>
+                    <div className="card" style={{ width: '20rem', marginBottom: '20px' }}>
+                        <img src={coding} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-title">Consulting</p>
+                            <p className="card-text">Introducing Templates</p>
+                            <p className="description-more"> With competition getting tougher in the private markets, becoming... </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Allblog
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,43 +1,14 @@
 import React from 'react';
 import '../styles/Blogs.css'
 import Blognavbar from './Blognavbar';
-import img2022 from '../imges/2022.jpg';
-import coding from '../imges/coding.jpg';
-import frag from '../imges/frag.jpg';
+import Allblog from './Allblog';
 
 const Blogs = () => {
   return (
     <>
       <h1 className='container' style={{ marginTop: '40px', marginBottom: '40px' }}>Latest Posts</h1>\
       <Blognavbar />
-      <div className='container my-5'>
-        <div className="cards">
-          <div className="card my-card" style={{width: '20rem', marginBottom: '20px' }}>
-            <img src={img2022} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <p className="card-title">Card title</p>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <p className="description-more"> Quick </p>
-            </div>
-          </div>
-          <div className="card" style={{width: '20rem', marginBottom: '20px' }}>
-            <img src={frag} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <p className="card-title">Card title</p>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <p className="description-more"> Quick </p>
-            </div>
-          </div>
-          <div className="card" style={{width: '20rem', marginBottom: '20px' }}>
-            <img src={coding} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <p className="card-title">Card title</p>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <p className="description-more"> Quick </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Allblog />
     </>
   )
 }
